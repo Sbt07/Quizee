0a5fba9338eda9683f66154315e673ef9f70443a
Sakshi Tiwari <93910804+Sbt07@users.noreply.github.com>
2024-06-29 21:28:05 +0530

    Add files via upload

---
+++ end.js
@@ -0,0 +1,34 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+const MAX_HIGH_SCORES = 5;
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveScoreBtn.addEventListener('click', saveHighScore); 
+
+function saveHighScore(e) {
+    e.preventDefault();
+
+    if (!username.value) {
+        console.log("Username is required.");
+        return;
+    }
+
+    const score = {
+        score: Math.floor(Math.random() * 100),
+        name: username.value
+    };
+    console.log("New score:", score);
+    highScores.push(score);
+    highScores.sort((a, b) => b.score - a.score);
+    highScores.splice(MAX_HIGH_SCORES); 
+    localStorage.setItem("highScores", JSON.stringify(highScores));
+    window.location.assign("./index.html");
+}
